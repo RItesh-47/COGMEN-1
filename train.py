e543938cb0733c852574b8e4e1e512c1bdfd7af2
RItesh-47 <riteshsjb@gmail.com>
2024-06-08 00:48:10 +0530

    fix

--- train.py
+++ train.py
@@ -52,14 +52,15 @@ def main(args):
         if args.transformers:
             args.data = os.path.join(
                 args.data_dir_path,
-                args.dataset,
+                # args.dataset,
                 "transformers",
                 "data_" + args.dataset + ".pkl",
             )
             print(os.path.join(args.data_dir_path, args.dataset, "transformers"))
         else:
             args.data = os.path.join(
-                args.data_dir_path, args.dataset, "data_" + args.dataset + ".pkl"
+                # args.data_dir_path, args.dataset, "data_" + args.dataset + ".pkl"
+                args.data_dir_path, "data_" + args.dataset + ".pkl"
             )
 
     # load data
